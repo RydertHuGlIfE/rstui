d9de69f55be9e45f218d03cea8a33db5ed30bec7
RydertHuGlIfE <varunchauhan2001dma@gmail.com>
2026-02-28 22:37:03 +0530

    added basic loading indication

--- main.py
+++ main.py
@@ -2,17 +2,36 @@ import ui
 import connector
 import scp_handler
 from colorama import Fore, Style
-import threading 
+import threading
+import time,sys
 
+
+spinchar = ['|', '/', '-', '\\']
 
 def main():
     ui.print_ban()
     user, host, password = connector.getconn()
-    t1 = threading.Thread(target=connector.test_ssh, args=(user, host, password))
+
+    result = []
+    def ssh_wrapper():
+        result.append(connector.test_ssh(user, host, password))
+
+    t1 = threading.Thread(target=ssh_wrapper)
     t1.start()
 
-    if t1.is_alive():
-        t1.join()
+    i = 0
+    while t1.is_alive():
+        sys.stdout.write("\r" + "Connecting.... " + spinchar[i % 4])
+        sys.stdout.flush()
+        time.sleep(0.2)
+        i += 1
+
+    sys.stdout.write("\r" + " " * 20 + "\r")
+    sys.stdout.flush()
+
+    t1.join()
+
+    if result[0]:
         print(Fore.GREEN + "Connected!" + Style.RESET_ALL)
         scp_handler.smenu(user, host, password)
     else:
