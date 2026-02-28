6a6737a692c8d679399239d57c6733539b414999
RydertHuGlIfE <varunchauhan2001dma@gmail.com>
2026-02-28 22:23:01 +0530

    added threading as prep for loading bar

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import ui
 import connector
 import scp_handler
 from colorama import Fore, Style
+import threading 
 
 
 def main():
     ui.print_ban()
     user, host, password = connector.getconn()
+    t1 = threading.Thread(target=connector.test_ssh, args=(user, host, password))
+    t1.start()
 
-    if connector.test_ssh(user, host, password):
+    if t1.is_alive():
+        t1.join()
         print(Fore.GREEN + "Connected!" + Style.RESET_ALL)
         scp_handler.smenu(user, host, password)
     else:
